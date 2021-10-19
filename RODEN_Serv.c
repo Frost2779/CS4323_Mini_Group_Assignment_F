b429d70b4cd591b588c27e273053dc6e7b71c533
Jeff Roden <roden80@gmail.com>
2021-10-19 14:30:04 -0400

    Server complete and working

--- RODEN_Serv.c
+++ RODEN_Serv.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <pthread.h>
+#include <stdint.h>
 #include "socketConnection.h"
 #include "linkedList.h"
+#include "handleClientTraffic.h"
 
 #define maxClients 3
 #define maxThreads 3
+#define convertIntToVoidPtr(i) (void*)(uintptr_t)(i)
 
 /*Thread Pool Vars*/
 pthread_t poolOfThreads[maxThreads];
@@ -16,41 +17,39 @@ pthread_mutex_t queLock = PTHREAD_MUTEX_INITIALIZER;
 
 struct linkedList_t* clientQue;
 
+
 /********************
 Function: threadTask()
 Purpose:  function that each thread in the thread pool will be executing.
 Return Type: void
 ********************/
 void * threadTask(void *arg) {
-    void* clientSock;
+    struct socket_t* clientSocket;
     while(1) {
         //What needs to happen:
         pthread_mutex_lock(&queLock); // turn on mutex lock
         // if client que is empty, then wait with conditional //
         if (clientQue->count == 0) {
             pthread_cond_wait(&threadConditional, &queLock);
-            clientSock = linkedListPop(clientQue); // get client Socket and remove from que
+            clientSocket = (struct socket_t*)linkedListPop(clientQue); // get client Socket and remove from que
         } else {
-            clientSock = linkedListPop(clientQue); // get client Socket and remove from que
+            clientSocket = (struct socket_t*)linkedListPop(clientQue); // get client Socket and remove from que
         }
         pthread_mutex_unlock(&queLock); // unlock mutex
-        printf("popped socket : %d\n", *(int *)clientSock);
+        printf("Handling Client on socket : %d\n", clientSocket->socketFD);
 
         // Receive from client what thread will be doing //
-        // 1. receive an encrypted message, store into a file, then send back the filename and key
-        // 2. receive a file name and send back an encrypted message
+        handleClientTraffic(clientSocket->socketFD);
     }
 }
 
-
 int main() {
     //Server Vars
-    struct sockaddr_in serverAddress;
-    struct sockaddr_in clientAddress;
-    int clientLength, servSock, clientSock;
-    //create a linked list to store client connections, needs to be
-    // global since threads share data segment.
+    struct socket_t *serverSocket, *clientSocket;
+    void *cSock;
 
+    //init linked list to store client connections, declared in
+    // global since threads share data segment.
     clientQue = mallocLinkedList();
 
     //Creating threads in the poolOfThreads array
@@ -58,44 +57,21 @@ int main() {
         pthread_create(&poolOfThreads[i], NULL, threadTask, NULL);
     }
 
-    //Populate serverAddress
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);  //Setting port
-
-    //Create server socket
-    if((servSock = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
-        printf("error creating server socket\n");
-    } else {
-        printf("server socket created\n");
-    }
-
-    //Bind Socket to port
-    if((bind(servSock,(struct sockaddr *)&serverAddress, sizeof(serverAddress))) < 0 ) {
-        printf("error binding server socket\n");
-    } else {
-        printf("server socket bound\n");
-    }
-
-    //Listen and set max number of clients to accept
-    if(listen(servSock, maxClients) < 0 ) {
-        printf("error listening\n");
-    } else {
-        printf("Listening for %d clients on port %d\n", maxClients, PORT);
-    }
-
+    //Setup server TCP
+    printf("Initializing Server...\n");
+    serverSocket = createSocket(ADDRESS, PORT);
+    bindSocket(serverSocket);
+    listenSocket(serverSocket, maxClients);
+    printf("Listening for %d clients on port %d\n", maxClients, PORT);
 
     //Handle client traffic
     while (1) {
-        clientLength = sizeof(struct sockaddr_in);
-        if((clientSock = accept(servSock, (struct sockaddr *)&clientAddress,(socklen_t*)&clientLength)) < 0 ) {
-            printf("!!!error connecting client!!!\n");
-        } else {
-            printf("...client connected...\n");
+        if ((clientSocket = acceptSocket(serverSocket)) != NULL) {
+            printf("...Client connected...\n");
         }
         //Once a client connects, pass it to a thread from the threadpool
         pthread_mutex_lock(&queLock);              //lock que to prevent race condition
-        linkedListAppend(clientQue, &clientSock);  //Add client's socket to the clientQue
+        linkedListAppend(clientQue, clientSocket);        //Add client's socket to the clientQue
         pthread_cond_signal(&threadConditional);   //signal a thread to work
         pthread_mutex_unlock(&queLock);            //unlock client que
     }
